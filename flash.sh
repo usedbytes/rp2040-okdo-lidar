a0cdf77e08250d6a03709f49bdb4cc98372cee8c
Brian Starkey <stark3y@gmail.com>
2024-04-30 19:48:30 +0100

    Switch programmer to cmsis-dap (RPi debug probe)

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -s tcl -c "program ${1} verify reset exit"
+openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -s tcl -c "program ${1} verify reset exit"
--- openocd.sh
+++ openocd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -s tcl
+openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -s tcl
