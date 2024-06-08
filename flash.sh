647917f3b8b0da8141bbb743e55186e2954306ee
Brian Starkey <stark3y@gmail.com>
2024-06-08 15:17:11 +0100

    Make flash.sh faster

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -s tcl -c "program ${1} verify reset exit"
+openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -s tcl -c "adapter_khz 1000" -c "program ${1} verify reset exit"
