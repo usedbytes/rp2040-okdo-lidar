abb598289ea326ab1360965011be9811a5472204
Brian Starkey <stark3y@gmail.com>
2024-06-12 22:43:56 +0100

    Move tools to tools/

--- tools/usb_raw.py
+++ tools/usb_raw.py
@@ -6,9 +6,6 @@ dev = usb.core.find(idVendor=0x1209, idProduct=0x0001)
 if dev is None:
     raise ValueError('device not found')
 
-#if dev.is_kernel_driver_active(0):
-#    dev.detach_kernel_driver(0)
-
 dev.set_configuration()
 cfg = dev.get_active_configuration()
 
